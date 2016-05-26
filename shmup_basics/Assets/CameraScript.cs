7542828005413fa5bcb011dc5c28b99ae552878c
Clement-R <rondeau.clement@gmail.com>
2016-05-26 14:22:51 +0200

    Init

---
+++ shmup_basics/Assets/CameraScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public float textureSize = 1f;
+
+    void Awake () {
+        float unitsPerPixel = 1f / textureSize;
+        Camera.main.orthographicSize = (Screen.height / 2f) * unitsPerPixel;
+    }
+}
