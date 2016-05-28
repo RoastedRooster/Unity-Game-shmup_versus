5132f040daa84a468688496e74ea91b1f8f5660e
Clement-R <rondeau.clement@gmail.com>
2016-05-28 17:04:57 +0200

    Add bullet and physic material for walls

---
+++ shmup_basics/Assets/scripts/ShooterBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterBehavior : MonoBehaviour {
+
+    public int direction;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
