4467a767c2dfee7f7171c7b8952b854ed87983c6
Clement-R <rondeau.clement@gmail.com>
2016-05-31 13:24:33 +0200

    Add first tween script, testing DOTween

---
+++ shmup_basics/Assets/enemies/GoForwardScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GoForwardScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.DOMoveY(transform.position.y - 150, 2);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
