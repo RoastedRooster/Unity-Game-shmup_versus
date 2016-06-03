dcfb9b3efe535ba99b2ac59a09f4f48a4c31cd05
Nujerk <junkerlucas@gmail.com>
2016-06-03 16:06:54 +0200

    Apply GoKit on GoForward

--- shmup_basics/Assets/enemies/GoForwardScript.cs
+++ shmup_basics/Assets/enemies/GoForwardScript.cs
@@ -8,20 +8,36 @@ public class GoForwardScript : MonoBehaviour {
      public float MoveSpeed = 1;
      public float curvMag = 1;
 
-     float fTime = 0;
+
+    protected AbstractGoTween _tween;
+
+    float fTime = 0;
      Vector3 vLastPos = Vector3.zero;
      Vector3 svLastPos = Vector3.zero;
 
      // Use this for initialization
      void Start ()
      {
+
+        var path = new GoSpline("route52497");
+        var move = Go.to(transform, 4f, new GoTweenConfig()
+            .positionPath(path, true));
+
+        var move2 = Go.to(transform, 4f, new GoTweenConfig().positionPath(path, true));
+
+        var chain = new GoTweenChain(new GoTweenCollectionConfig().setIterations(-1));
+        chain.append(move);
+        chain.append(move2);
+
+        _tween = chain;
+        _tween.play();
         /* DOTween tests */
         // DOMove(Vector2 to, float duration, bool snapping)
-        transform.DOMoveY(transform.position.y - 150, 2);
+        //transform.DOMoveY(transform.position.y - 150, 2);
         // Sequence mySequence = DOTween.Sequence();
         // mySequence.Append
 
-        svLastPos = transform.position;
+        //svLastPos = transform.position;
 
         /* SIN movement with DOTween */
         // First version : Using DOPath function, may break physic collision
@@ -29,18 +45,19 @@ public class GoForwardScript : MonoBehaviour {
 
         // Second version : Using DOMove and give it the waypoints one by one with a corountine
         // DOMove(Vector2 to, float duration, bool snapping)
-     }
+    }
 
      // Update is called once per frame
      void Update ()
      {
-        vLastPos = transform.position;
 
-        /* SIN movement working */
-        transform.position = svLastPos + new Vector3(MoveSpeed * Time.time,
-                                                     (Mathf.Sin(Time.time * CurveSpeed) * curvMag) - ((MoveSpeed * Time.time) / 2),
-                                                     0.0f);
+        //vLastPos = transform.position;
 
-        Debug.DrawLine(vLastPos, transform.position, Color.green, 100);
+        ///* SIN movement working */
+        //transform.position = svLastPos + new Vector3(MoveSpeed * Time.time,
+        //                                             (Mathf.Sin(Time.time * CurveSpeed) * curvMag) - ((MoveSpeed * Time.time) / 2),
+        //                                             0.0f);
+
+        //Debug.DrawLine(vLastPos, transform.position, Color.green, 100);
      }
 }
