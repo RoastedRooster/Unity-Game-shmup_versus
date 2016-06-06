027aa581aea7b8c2b06249df4e975a457a7fceb2
Clement-R <rondeau.clement@gmail.com>
2016-06-06 23:15:52 +0200

    Add new weapon for the enemy and capacity to shoot

--- shmup_basics/Assets/EnemyBehavior.cs
+++ shmup_basics/Assets/EnemyBehavior.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class EnemyBehavior : MonoBehaviour {
 
     int health = 2;
+    private ShooterBehavior weapon;
+
+    void Awake() {
+        weapon = gameObject.GetComponentInChildren<ShooterBehavior>();
+    }
 
     void Update() {
         if(health <= 0) {
             GameObject.Destroy(gameObject);
         }
+
+        weapon.fire();
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
-        if(coll.tag == "bullet") {
+        if(coll.tag == "bullet" && coll.gameObject.layer == LayerMask.NameToLayer("bullet_player")) {
             health--;
 			GameObject.Destroy (coll.gameObject);
         }
--- shmup_basics/Assets/enemies/GoForwardScript.cs
+++ shmup_basics/Assets/enemies/GoForwardScript.cs
@@ -26,7 +26,7 @@ public class GoForwardScript : MonoBehaviour {
         var move2 = Go.to(transform, 1f, new GoTweenConfig().positionPath(path, true));
 
         var path2 = new GoSpline("forward_top_bot");
-        var move3 = Go.to(transform, 2f, new GoTweenConfig().positionPath(path2, true));
+        var move3 = Go.to(transform, 5f, new GoTweenConfig().positionPath(path2, true));
 
         var chain = new GoTweenChain(new GoTweenCollectionConfig().setIterations(1));
 
