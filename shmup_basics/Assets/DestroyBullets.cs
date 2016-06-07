c9aa7cd0a067f18b5b3174c6c223b73c9cc651b7
Scarounet <rondeau.clement@gmail.com>
2016-06-07 09:32:48 +0200

    Add bullet collision with player and start to work with enemy destruction when hitting player's base

--- shmup_basics/Assets/DestroyBullets.cs
+++ shmup_basics/Assets/DestroyBullets.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class DestroyBullets : MonoBehaviour {
     
     void OnTriggerEnter2D(Collider2D coll) {
-        if (coll.transform.tag == "bullet") {
-            Destroy(coll.gameObject);
-        }
+		if (coll.transform.tag == "bullet") {
+			Destroy (coll.gameObject);
+		}
     }
+
+	void OnCollisionExit2D(Collision2D coll) {
+		Debug.Log ("Destroy you know");
+		if (coll.transform.tag == "Enemy") {
+			Destroy (coll.gameObject);
+		}
+	}
 }
---
+++ shmup_basics/Assets/scripts/PlayerBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public int ControllerIndex;
+    public float hMaxSpeed = 175f;
+    public float vMaxSpeed = 150f;
+
+    private Rigidbody2D rb2d;
+    private ShooterBehavior weapon;
+
+    public void setControllerIndex(int i) {
+        ControllerIndex = i;
+    }
+
+    public int getControllerIndex() {
+        return ControllerIndex;
+    }
+
+    void Awake() {
+        rb2d = GetComponent<Rigidbody2D>();
+        weapon = gameObject.GetComponentInChildren<ShooterBehavior>();
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxisRaw("Horizontal_" + ControllerIndex);
+        float v = Input.GetAxisRaw("Vertical_" + ControllerIndex);
+
+        rb2d.velocity = new Vector2(h * hMaxSpeed, v * vMaxSpeed);
+
+        if (Input.GetButton("Fire_" + ControllerIndex)) {
+            weapon.fire();
+        }
+    }
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.transform.tag == "bullet") {
+			GameObject.Destroy (coll.gameObject);
+		}
+	}
+}
