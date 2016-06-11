1fbda68d6d0d0dc8155c0ccfdafb2899e133ec16
Clement-R <rondeau.clement@gmail.com>
2016-06-11 17:18:48 +0200

    Add debug points for option drop system

--- shmup_basics/Assets/scripts/FieldGameManager.cs
+++ shmup_basics/Assets/scripts/FieldGameManager.cs
@@ -12,18 +12,33 @@ public class FieldGameManager : MonoBehaviour {
 	void Start () {
 		SpriteRenderer fieldSprite = playerField.GetComponent<SpriteRenderer> ();
 		startPoint = new Vector2(fieldSprite.bounds.extents.x, 0.0f);
-		// endPoint = ;
+        float width = fieldSprite.bounds.size.x;
+        float height = fieldSprite.bounds.size.y;
 
-		/*
-		sprite.bounds.extents.x; //Distance to the right side, from your center point
-		-sprite.bounds.extents.x //Distance to the left side
-		sprite.bounds.extents.y //Distance to the top
-		-sprite.bounds.extents.y //Distance to the bottom
-		*/
-	}
+        startPoint = new Vector2(playerField.transform.position.x - width/2,
+                                 playerField.transform.position.y + height/2);
+
+        endPoint = new Vector2(playerField.transform.position.x + width / 2,
+                               playerField.transform.position.y + height / 2);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Debug.DrawLine(new Vector2(startPoint.x -5, startPoint.y + 5),
+                       new Vector2(startPoint.x + 5, startPoint.y - 5),
+                       Color.red);
+
+        Debug.DrawLine(new Vector2(startPoint.x + 5, startPoint.y + 5),
+                       new Vector2(startPoint.x - 5, startPoint.y - 5),
+                       Color.red);
+
+        Debug.DrawLine(new Vector2(endPoint.x - 5, endPoint.y + 5),
+                       new Vector2(endPoint.x + 5, endPoint.y - 5),
+                       Color.red);
+
+        Debug.DrawLine(new Vector2(endPoint.x + 5, endPoint.y + 5),
+                       new Vector2(endPoint.x - 5, endPoint.y - 5),
+                       Color.red);
+    }
 }
