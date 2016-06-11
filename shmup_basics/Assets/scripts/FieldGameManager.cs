2746960c2b5d3e141d1ce814eaffb5ccae9acb39
Scarounet <rondeau.clement@gmail.com>
2016-06-11 14:32:25 +0200

    Start to implement bonus system

---
+++ shmup_basics/Assets/scripts/FieldGameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldGameManager : MonoBehaviour {
+
+	public GameObject playerField;
+
+	private Vector2 startPoint;
+	private Vector2 endPoint;
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer fieldSprite = playerField.GetComponent<SpriteRenderer> ();
+		startPoint = new Vector2(fieldSprite.bounds.extents.x, 0.0f);
+		// endPoint = ;
+
+		/*
+		sprite.bounds.extents.x; //Distance to the right side, from your center point
+		-sprite.bounds.extents.x //Distance to the left side
+		sprite.bounds.extents.y //Distance to the top
+		-sprite.bounds.extents.y //Distance to the bottom
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
