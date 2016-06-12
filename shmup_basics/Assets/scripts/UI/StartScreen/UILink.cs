f5a2d6bbf703cb1db8966e0a13d4ccb84ee07b07
Nujerk <junkerlucas@gmail.com>
2016-06-12 17:04:14 +0200

    Starting screen scene and UI

---
+++ shmup_basics/Assets/scripts/UI/StartScreen/UILink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace rr.ui {
+
+    public class UILink : MonoBehaviour {
+
+        public string sceneToLoad;
+
+        public void Activate() {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+    }
+
+}
---
+++ shmup_basics/Assets/scripts/UI/StartScreen/UITransformEffect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rr.ui {
+
+    public class UITransformEffect : MonoBehaviour {
+        public Vector2 from;
+        public Vector2 to;
+        public float duration;
+
+        void Update() {
+            var currentPos = (Vector2)gameObject.transform.position;
+
+            if (currentPos == to) {
+                enabled = false;
+                return;
+            }
+
+            var speed = Vector2.Distance(from, to) / duration;
+            var maxDistance = Time.deltaTime * speed;
+
+            gameObject.transform.position = Vector2.MoveTowards(currentPos, to, maxDistance);
+        }
+    }
+
+}
