c085ea9c06bb5953e6828ccfd93faa48385aec60
Clement-R <rondeau.clement@gmail.com>
2016-06-13 08:26:41 +0200

    Start to implement power up bonus/malus system

---
+++ shmup_basics/Assets/scripts/Power Up/PowerUpBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class PowerUpBehavior : MonoBehaviour {
+    public IPowerUp powerUp;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, -200);
+    }
+
+    public void activateBonus() {
+        powerUp.bonus();
+    }
+
+    public void activateMalus() {
+        powerUp.malus();
+    }
+}
---
+++ shmup_basics/Assets/scripts/Power Up/TestPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestPowerUp : MonoBehaviour, IPowerUp {
+
+    void IPowerUp.bonus() {
+        throw new NotImplementedException();
+    }
+
+    void IPowerUp.malus() {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
